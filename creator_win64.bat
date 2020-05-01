e833664c39895c7f7599fd670cbe774b78d67b58
Makopoppo <bokemako@gmail.com>
2020-05-01 15:32:36 +0900

    Please run this script on Native Tools Command Prompts for VS XXXX.

--- creator_win64.bat
+++ creator_win64.bat
@@ -1,15 +1,15 @@
 @echo off
-rem :: VS2015
-IF EXIST "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" (
-    call "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" amd64
-) else (
-    rem :: VS2017
-    rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
-    rem :: Simply run the script, as long as C:\flexandbison exists.
-    pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\"
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-    popd
-)
+rem :: rem :: VS2015
+rem :: IF EXIST "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" (
+rem ::     call "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" amd64
+rem :: ) else (
+rem ::     rem :: VS2017
+rem ::     rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
+rem ::     rem :: Simply run the script, as long as C:\flexandbison exists.
+rem ::     pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\"
+rem ::     call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+rem ::     popd
+rem :: )
 set PATH=%PATH%;C:\flexandbison
 nmake /F NMakefile
 mkdir binary\windows64\
