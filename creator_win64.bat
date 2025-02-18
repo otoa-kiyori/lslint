3d21a1032b6187503e91a17db69b24292aba4edb
Otoa <otoa.kiyori@gmail.com>
2025-02-18 01:23:53 -0700

    Update 20250218
    Updated creator_wwin64.bat to use VS2022 C++ build

--- creator_win64.bat
+++ creator_win64.bat
@@ -1,27 +1,22 @@
 @echo off
-rem :: Check for Visual Studio
-reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.14.0" >> nul 2>&1
-if not errorlevel 1 (  
-    rem :: VS2015
-    call "C:\Program Files (x86)\Microsoft Visual studio 14.0\VC\vcvarsall.bat" amd64
+rem :: Check for Visual Studio 2022
+
+reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.17.0" >> nul 2>&1
+if not errorlevel 1 (
+    rem :: VS2017
+    rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
+    pushd "C:\Program Files\Microsoft Visual Studio\2022\Community\"
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    popd
 ) else (
-    reg query "HKEY_CLASSES_ROOT\VisualStudio.DTE.15.0" >> nul 2>&1
-    if not errorlevel 1 ( 
-        rem :: VS2017
-        rem :: NOTE: The instructions contained in the NMakefile are no longer necessary with this method.
-        pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\"
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-        popd
-    ) else (
-        echo You do not appear to have Visual Studio 2015 or 2017 installed.
-        exit /b 1
-    )
-) 
+    echo "You do not appear to have Visual Studio 2022 (x64) Installed"
+    exit /b 1
+)
 
 if not exist C:\flexandbison\ ( echo "You need to have the Flex and Bison Packages, please see documentation." && exit /b 1 )
 
 set PATH=%PATH%;C:\flexandbison
 nmake /F NMakefile
-mkdir binary\windows64\
+mkdir binary\windows64\ >> nul 2>&1
 move lslint.exe binary\windows64\
 nmake /F NMakefile clean
